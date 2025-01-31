9beff1c2f30552385e00d1b164b23d34a714b641
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-01-31 12:03:05 +0300

    fix: update version to latest

--- lib/src/web/plugin_config.dart
+++ lib/src/web/plugin_config.dart
@@ -1,5 +1,5 @@
 class CountlyFlutterPluginConfig {
-  static const String SDK_VERSION_STRING = '24.11.2';
+  static const String SDK_VERSION_STRING = '25.1.0';
   static const String SDK_NAME = 'dart-flutterb-web';
   static const String WEB_SDK_URL = 'https://cdnjs.cloudflare.com/ajax/libs/countly-sdk-web/24.11.4/countly.min.js';
 }
