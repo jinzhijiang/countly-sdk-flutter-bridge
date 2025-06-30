8b19c1ad7bcad9cef840dab5e5c611bfafeebd63
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-06-30 12:34:04 +0300

    feat: move sbs related utils to related utils

--- example/integration_test/sbs_tests/SBS_000_base_test.dart
+++ example/integration_test/sbs_tests/SBS_000_base_test.dart
@@ -6,6 +6,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 import '../utils.dart';
+import 'sbs_utils.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
---
+++ example/integration_test/sbs_tests/sbs_utils.dart
@@ -0,0 +1,118 @@
+import 'dart:convert';
+import 'package:countly_flutter/countly_flutter.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../event_tests/event_utils.dart';
+import '../utils.dart';
+
+/// Validates the immediate counts in the request array.
+/// This function checks the number of immediate methods recorded in the request array
+/// and compares them with the expected counts provided in the `immediates` map.
+/// It expects the keys in the `immediates` map to be the method names (like 'hc', 'sc', 'feedback', etc.)
+/// and the values to be the expected counts of those methods.
+/// @param immediates A map where keys are the names of the immediate methods and values are the expected counts. like {'hc': 1, 'sc': 1, 'feedback': 1, 'queue': 2, 'ab': 1, 'ab_opt_out': 1, 'rc': 1}
+/// @param requestArray The array of requests to validate against.
+void validateImmediateCounts(Map<String, int> immediates, List<Map<String, List<String>>> requestArray) {
+  Map<String, int> actualImmediates = <String, int>{};
+
+  // key is method values are the switch cases
+  for (var request in requestArray) {
+    if (request.containsKey('method')) {
+      String method = request['method']![0];
+      actualImmediates[method] = (actualImmediates[method] ?? 0) + 1;
+    } else if (request.containsKey('hc')) {
+      actualImmediates['hc'] = (actualImmediates['hc'] ?? 0) + 1;
+    }
+  }
+
+  expect(actualImmediates.length, immediates.length, reason: 'Mismatch in number of immediate methods');
+  // Validate the counts
+  for (var entry in immediates.entries) {
+    expect(actualImmediates[entry.key], entry.value, reason: 'Mismatch for method ${entry.key}');
+  }
+}
+
+/// Validates the internal event counts in the request array.
+/// This function checks the number of internal events recorded in the request array
+/// and compares them with the expected counts provided in the `internalEventsCounts` map.
+/// It expects the keys in the `internalEventsCounts` map to be not prefixed with '[CLY]_'.
+/// The function will throw an error if the counts do not match.
+/// @param internalEventsCounts A map where keys are the names of the internal events (without '[CLY]_') and values are the expected counts. like {'orientation': 1, 'view': 6}
+/// @param requestArray The array of requests to validate against.
+///
+void validateInternalEventCounts(Map<String, int> internalEventsCounts, List<Map<String, List<String>>> requestArray) {
+  Map<String, int> actualCounts = <String, int>{};
+
+  // key is method values are the switch cases
+  for (var request in requestArray) {
+    if (request.containsKey('events')) {
+      List<Map<String, dynamic>> events = (jsonDecode(request['events']![0]) as List).cast<Map<String, dynamic>>();
+      for (var event in events) {
+        if (event['key'].toString().startsWith('[CLY]')) {
+          actualCounts[event['key']] = (actualCounts[event['key']] ?? 0) + 1;
+        }
+      }
+    }
+  }
+
+  expect(actualCounts.length, internalEventsCounts.length, reason: 'Mismatch in number of internal event methods actual: $actualCounts, expected: $internalEventsCounts');
+  // Validate the counts
+  for (var entry in internalEventsCounts.entries) {
+    expect(actualCounts['[CLY]_${entry.key}'], entry.value, reason: 'Mismatch for method ${entry.key}');
+  }
+}
+
+/// Calls all features of Countly SDK to ensure they are working correctly.
+/// This includes events, views, sessions, user location, user profile, crash, feedback widgets, remote config, A/B testing, consent, and content zone.
+/// It also includes the things that are affected by the SDK internal limits, such as truncable events
+/// This function is used in integration tests to validate the functionality of the Countly SDK with the SBS
+/// At the end of the function, it triggers sending requests to the queue and waits for 10 seconds to ensure all requests are sent and queues are empty
+Future<void> callAllFeatures() async {
+  await Countly.getAvailableFeedbackWidgets();
+
+  await Countly.giveAllConsent();
+  await Countly.instance.sessions.beginSession();
+  await createTruncableEvents();
+  await generateEvents();
+  await Countly.setUserLocation(countryCode: 'TR', city: 'Istanbul', gpsCoordinates: '41.0082,28.9784', ipAddress: '10.2.33.12');
+  await Countly.instance.events.recordEvent('Event With Sum And Segment', {'Country': 'Turkey', 'Age': 28884}, 1, 0.99); // not legacy code
+  Map<String, Object> segmentation = {
+    "country": "Germany",
+    "app_version": "1.0",
+    "rating": 10,
+    "precision": 324.54678,
+    "timestamp": 1234567890,
+    "clicked": false,
+    "languages": ["en", "de", "fr"],
+    "sub_names": ["John", "Doe", "Jane"]
+  };
+  final String? viewID = await Countly.instance.views.startView("Dashboard", segmentation);
+
+  // IMMEDIATE CALLS
+  await Countly.instance.content.enterContentZone();
+  await Countly.instance.remoteConfig.downloadAllKeys((rResult, error, fullValueUpdate, downloadedValues) {
+    if (rResult == RequestResult.success) {
+      // do sth
+    } else {
+      // do sth
+    }
+  });
+
+  await Countly.instance.remoteConfig.enrollIntoABTestsForKeys(['key1', 'key2']);
+  await Countly.instance.remoteConfig.exitABTestsForKeys(['key1', 'key2']);
+
+  // END IMMEDIATE CALLS
+  await Countly.reportFeedbackWidgetManually(CountlyPresentableFeedback('test', 'nps', 'test'), {}, {});
+
+  await Future.delayed(const Duration(seconds: 2));
+  await Countly.instance.sessions.updateSession();
+  await Countly.instance.views.stopViewWithID(viewID!);
+  await Countly.instance.content.refreshContentZone();
+
+  await Future.delayed(const Duration(seconds: 2));
+  await Countly.instance.sessions.endSession();
+
+  await Countly.instance.attemptToSendStoredRequests();
+  // check queues are empty and all requests are sent
+  await Future.delayed(const Duration(seconds: 10));
+}
