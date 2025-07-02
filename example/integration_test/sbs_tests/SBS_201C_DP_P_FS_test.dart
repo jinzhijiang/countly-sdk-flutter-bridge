758232b9a7c294736ea61751fe74298577167bf6
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-07-02 15:48:36 +0300

    feat: dp p fs test

--- example/integration_test/sbs_tests/SBS_201C_DP_P_FS_test.dart
+++ example/integration_test/sbs_tests/SBS_201C_DP_P_FS_test.dart
@@ -10,13 +10,19 @@ import 'sbs_utils.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-  testWidgets('SBS_201B_DP_P_test', (WidgetTester tester) async {
+  testWidgets('SBS_201C_DP_P_FS_test', (WidgetTester tester) async {
     List<Map<String, List<String>>> requestArray = <Map<String, List<String>>>[];
     createServer(requestArray, customHandler: (request, queryParams, response) async {
       Map<String, Object> responseJson = {'result': 'Success'};
       if (queryParams.containsKey('method')) {
         if (queryParams['method']!.first == 'feedback') {
           responseJson = {'result': []};
+        } else if (queryParams['method']!.first == 'sc') {
+          responseJson = {
+            'v': 1,
+            't': 1750748806695,
+            'c': {'crt': false, 'vt': false, 'st': true, 'cr': false, 'cet': false, 'log': true, 'dort': 21, 'lkl': 67, 'lvs': 79, 'lsv': 90, 'lbc': 88, 'ltlpt': 34, 'ltl': 250, 'rcz': true, 'bom': true}
+          };
         }
       }
       response
@@ -41,10 +47,10 @@ void main() {
     expect(await getServerConfig(), {
       'v': 1,
       't': 1750748806695,
-      'c': {'crt': false, 'vt': false, 'st': true, 'cr': false, 'cet': true, 'log': false, 'dort': 12, 'lkl': 120, 'lvs': 255, 'lsv': 99, 'lbc': 99, 'ltlpt': 29, 'ltl': 199, 'rcz': true, 'bom': true}
+      'c': {'crt': false, 'vt': false, 'st': true, 'cr': false, 'cet': false, 'log': true, 'dort': 21, 'lkl': 67, 'lvs': 79, 'lsv': 90, 'lbc': 88, 'ltlpt': 34, 'ltl': 250, 'rcz': true, 'bom': true}
     });
 
-    validateRequestCounts({'events': 8, 'location': 3, 'crash': 0, 'begin_session': 1, 'end_session': 1, 'session_duration': 2, 'apm': 2, 'user_details': 1, 'consent': 0}, requestArray);
+    validateRequestCounts({'events': 1, 'location': 3, 'crash': 0, 'begin_session': 1, 'end_session': 1, 'session_duration': 2, 'apm': 2, 'user_details': 1, 'consent': 0}, requestArray);
     validateInternalEventCounts({'orientation': 1}, requestArray);
     validateImmediateCounts({'hc': 1, 'sc': 1, 'feedback': 1, 'queue': 2, 'ab': 1, 'ab_opt_out': 1, 'rc': 1}, requestArray);
   });
