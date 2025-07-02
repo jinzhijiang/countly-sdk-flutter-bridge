82dd61bba8fec15851accc4d689706baa2b9a270
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-07-02 11:20:28 +0300

    feat: backoff to the base test

--- example/integration_test/sbs_tests/SBS_000_base_test.dart
+++ example/integration_test/sbs_tests/SBS_000_base_test.dart
@@ -11,12 +11,20 @@ import 'sbs_utils.dart';
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   testWidgets('SBS_000_base', (WidgetTester tester) async {
+    int serverDelay = 0;
     List<Map<String, List<String>>> requestArray = <Map<String, List<String>>>[];
     createServer(requestArray, customHandler: (request, queryParams, response) async {
       Map<String, Object> responseJson = {'result': 'Success'};
       if (queryParams.containsKey('method') && queryParams['method']!.first == 'feedback') {
         responseJson = {'result': []};
       }
+
+      if (serverDelay > 0 && !queryParams.containsKey('events')) {
+        // this orientation check for avoiding delay on backoff check
+        // to validate end session sent after 60 seconds
+        await Future.delayed(Duration(seconds: serverDelay));
+      }
+
       response
         ..statusCode = HttpStatus.ok
         ..headers.contentType = ContentType.json
@@ -30,17 +38,28 @@ void main() {
     await callAllFeatures();
 
     print(requestArray);
-    expect(true, requestArray.any((item) => item.containsKey('events')));
-    expect(true, requestArray.any((item) => item.containsKey('location'))); //x2
-    expect(true, requestArray.any((item) => item.containsKey('crash'))); //x2
-    expect(true, requestArray.any((item) => item.containsKey('begin_session')));
-    expect(true, requestArray.any((item) => item.containsKey('end_session')));
-    expect(true, requestArray.any((item) => item.containsKey('session_duration') && !item.containsKey('end_session')));
-    expect(true, requestArray.any((item) => item.containsKey('session_duration') && item.containsKey('end_session')));
-    expect(true, requestArray.any((item) => item.containsKey('apm'))); //x2
-    expect(true, requestArray.any((item) => item.containsKey('user_details'))); //x2
-
+    List<String> RQ = await getRequestQueue();
+    List<String> EQ = await getEventQueue();
+    expect(RQ.length, 0);
+    expect(EQ.length, 0);
+    validateRequestCounts({'events': 2, 'location': 1, 'crash': 2, 'begin_session': 1, 'end_session': 1, 'session_duration': 2, 'apm': 2, 'user_details': 1}, requestArray);
     validateInternalEventCounts({'orientation': 1, 'view': 6}, requestArray);
     validateImmediateCounts({'hc': 1, 'sc': 1, 'feedback': 1, 'queue': 2, 'ab': 1, 'ab_opt_out': 1, 'rc': 1}, requestArray);
+
+    await Countly.instance.content.exitContentZone();
+    requestArray.clear();
+
+    serverDelay = 11;
+
+    await Countly.instance.sessions.beginSession();
+    await Countly.instance.sessions.endSession(); // this will be backed off for 60 seconds
+    await Countly.instance.attemptToSendStoredRequests();
+
+    validateRequestCounts({'begin_session': 1}, requestArray);
+    await Countly.instance.attemptToSendStoredRequests(); // this will not take effect
+    expect(requestArray.length, 1);
+
+    await Future.delayed(const Duration(seconds: 75));
+    validateRequestCounts({'begin_session': 1, 'end_session': 1}, requestArray);
   });
 }
