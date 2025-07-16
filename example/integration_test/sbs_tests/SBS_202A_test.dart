00b2260ee9cd0cb5239169666be112bfa4af9188
Arif Burak Demiray <burakdemiray09@hotmail.com>
2025-07-16 11:05:52 +0300

    fix: remove unused import

--- example/integration_test/sbs_tests/SBS_202A_test.dart
+++ example/integration_test/sbs_tests/SBS_202A_test.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:countly_flutter/countly_flutter.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
