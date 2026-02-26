f549b64f27693e80ab4fc4f00a9a254950e91ae3
Arif Burak Demiray <burakdemiray09@hotmail.com>
2026-02-26 14:12:38 +0300

    feat: upgrade versions

--- lib/src/web/plugin_config.dart
+++ lib/src/web/plugin_config.dart
@@ -1,6 +1,6 @@
 class CountlyFlutterPluginConfig {
-  static const String SDK_VERSION_STRING = '25.4.4';
+  static const String SDK_VERSION_STRING = '26.1.0';
   static const String SDK_NAME = 'dart-flutterb-web';
-  static const String WEB_SDK_VERSION = '25.4.2';
+  static const String WEB_SDK_VERSION = '26.1.0';
   static const String WEB_SDK_URL = 'https://cdn.jsdelivr.net/npm/countly-sdk-web@$WEB_SDK_VERSION/lib/countly.min.js';
 }
